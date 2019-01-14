b82b90892dcc90b285b46a01b20d2b4b630a90ef
Eugene Trubenok <etrubenok@gmail.com>
2019-01-14 17:08:18 +1100

    Adds more helper functions for Binance (primary key generation and hashing)

--- binance/helper.go
+++ binance/helper.go
@@ -1,7 +1,11 @@
 package binance
 
 import (
+	"crypto/sha1"
 	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/golang/glog"
 )
@@ -26,4 +30,36 @@ func GetBinanceDepthStreamMessage(message []byte) (*DepthStreamMessageInKafka, e
 		return nil, err
 	}
 	return &msg, nil
+}
+
+// GetTradeMessagePrimaryKey gets a concatenated string that forms from the message fields which identify the trade uniquely
+func GetTradeMessagePrimaryKey(trade *TradeStreamMessageInKafka) string {
+	fields := make([]string, 0)
+	fields = append(fields, trade.Exchange)
+	fields = append(fields, trade.Stream)
+	fields = append(fields, strconv.FormatInt(trade.EventTime, 10))
+	fields = append(fields, strconv.FormatInt(trade.RawMessage.Data.LowercaseT, 10))
+	return strings.Join(fields, "@")
+}
+
+// GetDepthMessagePrimaryKey gets a concatenated string that forms from the message fields which identify the depth message uniquely
+func GetDepthMessagePrimaryKey(trade *DepthStreamMessageInKafka) string {
+	fields := make([]string, 0)
+	fields = append(fields, trade.Exchange)
+	fields = append(fields, trade.Stream)
+	fields = append(fields, strconv.FormatInt(trade.EventTime, 10))
+	fields = append(fields, strconv.FormatInt(trade.RawMessage.Data.U, 10))
+	return strings.Join(fields, "@")
+}
+
+// GetKeyHash converts a plain string key into hash
+func GetKeyHash(key string) (string, error) {
+	h := sha1.New()
+	_, err := h.Write([]byte(key))
+	if err != nil {
+		glog.Errorf("GetKeyHash: cannot make a hash for key %s due to error %s", key, err)
+		return "", err
+	}
+	bs := h.Sum(nil)
+	return fmt.Sprintf("%x", bs), nil
 }
--- binance/helper_test.go
+++ binance/helper_test.go
@@ -30,3 +30,32 @@ func TestGetBinanceDepthStreamMessage(t *testing.T) {
 		assert.Equal(t, 3, len(msg.RawMessage.Data.LowercaseB))
 	}
 }
+
+func TestGetTradeMessagePrimaryKey(t *testing.T) {
+	msgStr, err := ioutil.ReadFile("./test-data/trade-msg.json")
+	assert.NoError(t, err)
+
+	msg, err := GetBinanceTradeStreamMessage(msgStr)
+	assert.NoError(t, err)
+
+	key := GetTradeMessagePrimaryKey(msg)
+	assert.Equal(t, "binance@btcusdt@trade@1547119839773@93684015", key)
+}
+
+func TestGetDepthMessagePrimaryKey(t *testing.T) {
+	msgStr, err := ioutil.ReadFile("./test-data/depth-msg.json")
+	assert.NoError(t, err)
+
+	msg, err := GetBinanceDepthStreamMessage(msgStr)
+	assert.NoError(t, err)
+
+	key := GetDepthMessagePrimaryKey(msg)
+	assert.Equal(t, "binance@ppteth@depth@1547119839095@49518600", key)
+}
+
+func TestGetHashKey(t *testing.T) {
+	h, err := GetKeyHash("binance@btcusdt@trade@1547119839773@93684015")
+	assert.NoError(t, err)
+
+	assert.Equal(t, "9f875ee4585152c34a56c23a29df253104b60898", h)
+}
