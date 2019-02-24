37251bc80f40a89088f1aecb73736e5a08ec6490
Eugene Trubenok <etrubenok@vlocity.com>
2019-02-24 21:36:42 +1100

    Adds bitfinex exchange

--- registry/registry.go
+++ registry/registry.go
@@ -6,6 +6,8 @@ import "fmt"
 func GetExchangeID(exchange string) (int, error) {
 	if exchange == "binance" {
 		return 1, nil
+	} else if exchange == "bitfinex" {
+		return 2, nil
 	}
 	return 0, fmt.Errorf("GetExchangeID: exchange: '%s' is not known", exchange)
 }
@@ -15,6 +17,8 @@ func GetExchangeNameByID(exchangeID int) (string, error) {
 	switch exchangeID {
 	case 1:
 		return "binance", nil
+	case 2:
+		return "bitfinex", nil
 	default:
 		return "", fmt.Errorf("GetExchangeNameByID does not know exchange with ID: %d", exchangeID)
 	}
